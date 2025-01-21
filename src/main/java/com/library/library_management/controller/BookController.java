871095901f781d7b9721769b43a5c5b60d908aff
SINCHANA S <sinchanas1308@gmail.com>
2025-01-21 15:20:01 +0530

    Added html pages

--- src/main/java/com/library/library_management/controller/BookController.java
+++ src/main/java/com/library/library_management/controller/BookController.java
@@ -2,6 +2,9 @@ package com.library.library_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -12,6 +15,17 @@ public class BookController {
     public String Home() {
         return "Home";
     }
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+
+    @GetMapping("/available_books")
+    public String getAllBooks() {
+        return  "BookList";
+    }
+    
+    
     
     
 }
